ce7f1c984c7e157d85bfbe659ee9ac9947c6931e
acalidonio <00363918@uca.edu.sv>
2025-11-17 05:12:30 -0600

    Refactorizar App para que incluya rutas protegidas y otras cosas

--- Frontend/laboratorio/src/App.jsx
+++ Frontend/laboratorio/src/App.jsx
@@ -1,14 +1,68 @@
+// Frontend/laboratorio/src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Protected from "./components/Protected";
+import "./App.css";
+import "./components/components.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import Login from "./components/Login.jsx";
+import Protected from "./components/Protected.jsx";
+import CustomerList from "./components/CustomerList.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import CreateSale from "./components/CreateSale.jsx";
+import SalesList from "./components/SalesList.jsx";
+import SalesReport from "./components/SalesReport.jsx";
+
+const Home = () => {
+  const token = localStorage.getItem("token");
+  // Si hay token, va a clientes, si no, a login.
+  return token ? <Navigate to="/customers" /> : <Navigate to="/login" />;
+};
 
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/protected" element={<Protected />} />
-    </Routes>
+    <div>
+      <nav className="app-nav">
+        <Link to="/login" className="app-nav-link">
+          Login
+        </Link>
+        <Link to="/customers" className="app-nav-link">
+          Clientes
+        </Link>
+        <Link to="/protected" className="app-nav-link">
+          Ruta protegida (Test)
+        </Link>
+        <Link to="/sales/new" className="app-nav-link">
+          Nueva venta
+        </Link>
+        <Link to="/sales/list" className="app-nav-link">
+          Lista de ventas
+        </Link>
+        <Link to="/sales/report" className="app-nav-link">
+          Reporte de ventas
+        </Link>
+      </nav>
+
+      <div className="app-content">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+
+          {/* Rutas protegidas */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/customers" element={<CustomerList />} />
+            <Route path="/protected" element={<Protected />} />
+            <Route path="/sales/new" element={<CreateSale />} />
+            <Route path="/sales/list" element={<SalesList />} />
+            <Route path="/sales/report" element={<SalesReport />} />
+          </Route>
+        </Routes>
+      </div>
+    </div>
   </Router>
 );
 
