f8a6c377736e3ed5511a17801ca502645e50c260
acalidonio <00363918@uca.edu.sv>
2025-11-17 05:11:41 -0600

    Refactorizar otros archivos

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,18 @@
 // index.js
-import app from './app.js';
-import { PORT } from './config.js';
-import { pool } from './database.js';
+import app from "./app.js";
+import { PORT } from "./config.js";
+import { pool } from "./database.js";
 
 async function main() {
   try {
-    await pool.query('SELECT NOW()');
-    console.log('Conexión exitosa a la base de datos.');
+    await pool.query("SELECT NOW()");
+    console.log("Conexión exitosa a la base de datos.");
 
     app.listen(PORT, () => {
       console.log(`Servidor corriendo en puerto ${PORT}`);
     });
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
+    console.error("Error al conectar a la base de datos:", error);
   }
 }
 
--- Backend/middlewares/verifyToken.js
+++ Backend/middlewares/verifyToken.js
@@ -1,19 +1,22 @@
 // middlewares/verifyToken.js
-import pkg from 'jsonwebtoken';
+import pkg from "jsonwebtoken";
+import { SECRET_KEY } from "../config.js";
 
 const { verify } = pkg;
 
-const JWT_SECRET = 'your_jwt_secret';
+const JWT_SECRET = SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Token no enviado o con formato incorrecto' });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res
+      .status(401)
+      .json({ message: "Token no enviado o con formato incorrecto" });
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Token inválido' });
+    if (err) return res.status(403).json({ message: "Token inválido" });
     req.user = user;
     next();
   });
